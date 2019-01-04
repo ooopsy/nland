63e01458d3fa64cb7f1f8e5554c5e403a2a8cddb
oopsy <jintian8899@gmail.com>
2019-01-04 16:35:02 +0900

    remove unnesseary

--- studyworkspace/tiny-facebook/src/main/java/com/b4u/tinyfacebook/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.b4u.tinyfacebook.repository;
-
-import com.b4u.tinyfacebook.entity.Post;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
